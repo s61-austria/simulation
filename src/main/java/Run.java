0f64565fa33749f6d4df1f551aa269a3f8d5009d
jandie <eidnaj@live.nl>
2018-05-08 16:58:12 +0200

    Getting vehicle lat lon implemented

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,8 +1,11 @@
 import de.tudresden.sumo.cmd.Edge;
 import de.tudresden.sumo.cmd.Simulation;
 import de.tudresden.sumo.cmd.Vehicle;
+import de.tudresden.ws.container.SumoPosition2D;
 import it.polito.appeal.traci.*;
 
+import java.awt.geom.Point2D;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class Run {
@@ -27,16 +30,20 @@ public class Run {
             conn.do_timestep();
 
             for(int i=0; i<3600; i++){
-                //get the CO2 emission for a specific edge
-
-                double co2 = (double) conn.do_job_get(Edge.getCO2Emission("gneE0"));
-                System.out.println("timestep: " + i + " " + co2 + " g/s");
-
-                //current simulation time
-                int simtime = (int) conn.do_job_get(Simulation.getCurrentTime());
 
                 //add new vehicle
-                conn.do_job_set(Vehicle.add("veh"+i, "car", "s1", simtime, 0, 13.8, (byte) 1));
+                Collection<String> vehicles = (Collection<String>) conn.do_job_get(Vehicle.getIDList());
+                if (vehicles.size() > 0) {
+                    System.out.println(Arrays.toString(vehicles.toArray()));
+
+                    SumoPosition2D pos =
+                            (SumoPosition2D) conn.do_job_get(
+                                    Vehicle.getPosition(vehicles.toArray()[0].toString()));
+
+                    pos = (SumoPosition2D) conn.do_job_get(Simulation.convertGeo(pos.x, pos.y, false));
+                    System.out.println("x: " + pos.x + " y: " + pos.y);
+                }
+
                 conn.do_timestep();
             }
 
