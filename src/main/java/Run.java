22485311a652e1f6e9420b927790759b8706a500
jandie <eidnaj@live.nl>
2018-05-08 14:45:57 +0200

    Running simulation, location conversion still needs to be fixed

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,28 +1,59 @@
-import it.polito.appeal.traci.SumoTraciConnection;
-import it.polito.appeal.traci.Vehicle;
+import it.polito.appeal.traci.*;
+import org.osgeo.proj4j.BasicCoordinateTransform;
+import org.osgeo.proj4j.CRSFactory;
+import org.osgeo.proj4j.CoordinateReferenceSystem;
+import org.osgeo.proj4j.ProjCoordinate;
 
+import java.awt.geom.Point2D;
 import java.util.Collection;
 
 public class Run {
     public static void main(String[] args) {
         SumoTraciConnection conn = new SumoTraciConnection(
-                "/maps/test/osm.sumocfg",  // config file
+                "./maps/test/osm.sumocfg",  // config file
                 12345                                  // random seed
         );
         try {
-            conn.runServer();
+            conn.runServer(true);
 
-            // the first two steps of this simulation have no vehicles.
-            conn.nextSimStep();
-            conn.nextSimStep();
+            System.out.println("Map bounds are: " + conn.queryBounds());
 
-            Collection<Vehicle> vehicles = conn.getVehicleRepository().getAll().values();
+            for (int i = 0; i < 1000000; i++) {
+                //Thread.sleep(1000);
+                int time = conn.getCurrentSimTime() / 1000;
+                Collection<Vehicle> vehicles = conn.getVehicleRepository().getAll().values();
 
-            Vehicle aVehicle = vehicles.iterator().next();
+                System.out.println("At time step " + time + ", there are "
+                        + vehicles.size() + " vehicles: " + vehicles);
 
-            System.out.println("Vehicle " + aVehicle
-                    + " will traverse these edges: "
-                    + aVehicle.getCurrentRoute());
+                if (vehicles.size() > 0) {
+                    //TODO: Location conversion from cartesian to lat lon still needs to be implemented correctly.
+                    Vehicle aVehicle = vehicles.iterator().next();
+                    double posX = aVehicle.getPosition().getX();
+                    double posY = aVehicle.getPosition().getY();
+                    double netOffSetX = -353027.49;
+                    double netOffSetY = -5295485.24;
+                    posX -= netOffSetX;
+                    posY -= netOffSetY;
+                    CRSFactory cf = new CRSFactory();
+                    CoordinateReferenceSystem crsSrc = cf.createFromName("EPSG:25832");
+                    CoordinateReferenceSystem crsDest = cf.createFromName("EPSG:4326");
+                    BasicCoordinateTransform basicTransform = new BasicCoordinateTransform(
+                            crsSrc, crsDest);
+                    ProjCoordinate pcSrc = new ProjCoordinate(posX, posY);
+                    ProjCoordinate pcDest = new ProjCoordinate();
+                    pcDest = basicTransform.transform(pcSrc, pcDest);
+                    Point2D newPosition = new Point2D.Double(pcDest.x, pcDest.y);
+
+                    System.out.println("Vehicle " + aVehicle
+                            + " x: "
+                            + newPosition.getX()
+                            + " x: "
+                            + newPosition.getY());
+                }
+
+                conn.nextSimStep();
+            }
 
             conn.close();
         }
