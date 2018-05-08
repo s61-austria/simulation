d3b214881dfd3257e1acb9af8f4db133e91eb097
jandie <eidnaj@live.nl>
2018-05-08 15:35:31 +0200

    traas added

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,64 +1,49 @@
+import de.tudresden.sumo.cmd.Edge;
+import de.tudresden.sumo.cmd.Simulation;
+import de.tudresden.sumo.cmd.Vehicle;
 import it.polito.appeal.traci.*;
-import org.osgeo.proj4j.BasicCoordinateTransform;
-import org.osgeo.proj4j.CRSFactory;
-import org.osgeo.proj4j.CoordinateReferenceSystem;
-import org.osgeo.proj4j.ProjCoordinate;
 
-import java.awt.geom.Point2D;
 import java.util.Collection;
 
 public class Run {
+
+    static String sumo_bin = "C:\\Program Files (x86)\\DLR\\Sumo\\bin\\sumo-gui.exe";
+    static final String config_file = "C:\\Users\\Jandie\\Desktop\\simulation\\maps\\test\\osm.sumocfg";
+
     public static void main(String[] args) {
-        SumoTraciConnection conn = new SumoTraciConnection(
-                "./maps/test/osm.sumocfg",  // config file
-                12345                                  // random seed
-        );
-        try {
-            conn.runServer(true);
 
-            System.out.println("Map bounds are: " + conn.queryBounds());
+        //start Simulation
+        SumoTraciConnection conn = new SumoTraciConnection(sumo_bin, config_file);
 
-            for (int i = 0; i < 1000000; i++) {
-                //Thread.sleep(1000);
-                int time = conn.getCurrentSimTime() / 1000;
-                Collection<Vehicle> vehicles = conn.getVehicleRepository().getAll().values();
+        //set some options
+        conn.addOption("step-length", "0.1"); //timestep 1 second
 
-                System.out.println("At time step " + time + ", there are "
-                        + vehicles.size() + " vehicles: " + vehicles);
+        try{
 
-                if (vehicles.size() > 0) {
-                    //TODO: Location conversion from cartesian to lat lon still needs to be implemented correctly.
-                    Vehicle aVehicle = vehicles.iterator().next();
-                    double posX = aVehicle.getPosition().getX();
-                    double posY = aVehicle.getPosition().getY();
-                    double netOffSetX = -353027.49;
-                    double netOffSetY = -5295485.24;
-                    posX -= netOffSetX;
-                    posY -= netOffSetY;
-                    CRSFactory cf = new CRSFactory();
-                    CoordinateReferenceSystem crsSrc = cf.createFromName("EPSG:25832");
-                    CoordinateReferenceSystem crsDest = cf.createFromName("EPSG:4326");
-                    BasicCoordinateTransform basicTransform = new BasicCoordinateTransform(
-                            crsSrc, crsDest);
-                    ProjCoordinate pcSrc = new ProjCoordinate(posX, posY);
-                    ProjCoordinate pcDest = new ProjCoordinate();
-                    pcDest = basicTransform.transform(pcSrc, pcDest);
-                    Point2D newPosition = new Point2D.Double(pcDest.x, pcDest.y);
+            //start TraCI
+            conn.runServer();
 
-                    System.out.println("Vehicle " + aVehicle
-                            + " x: "
-                            + newPosition.getX()
-                            + " x: "
-                            + newPosition.getY());
-                }
+            //load routes and initialize the simulation
+            conn.do_timestep();
 
-                conn.nextSimStep();
+            for(int i=0; i<3600; i++){
+                //get the CO2 emission for a specific edge
+
+                double co2 = (double) conn.do_job_get(Edge.getCO2Emission("gneE0"));
+                System.out.println("timestep: " + i + " " + co2 + " g/s");
+
+                //current simulation time
+                int simtime = (int) conn.do_job_get(Simulation.getCurrentTime());
+
+                //add new vehicle
+                conn.do_job_set(Vehicle.add("veh"+i, "car", "s1", simtime, 0, 13.8, (byte) 1));
+                conn.do_timestep();
             }
 
+            //stop TraCI
             conn.close();
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
+
+        }catch(Exception ex){ex.printStackTrace();}
+
     }
 }
